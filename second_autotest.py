506d93586183452bf46fb567837f14e789c9fd54
DenWertoo <aldoraygreat@yandex.ru>
2015-08-03 16:35:13 +0300

    второй автотест (без удаления групп)

--- second_autotest.py
+++ second_autotest.py
@@ -10,12 +10,12 @@ def is_alert_present(wd):
     except:
         return False
 
-class first_autotest(unittest.TestCase):
+class second_autotest(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_first_autotest(self):
+    def test_second_autotest(self):
         success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
@@ -27,29 +27,32 @@ class first_autotest(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("EVEV")
+        wd.find_element_by_name("group_name").send_keys("dwq3232")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").send_keys("\\51")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("EVVE")
+        wd.find_element_by_name("group_header").send_keys("523f23tg34")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("EVEVWV")
+        wd.find_element_by_name("group_footer").send_keys("3g434g34g43g")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/span[5]/input").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/span[5]/input").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/span[4]/input").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/span[4]/input").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/span[3]/input").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/span[3]/input").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/span[2]/input").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/span[2]/input").click()
-        if not wd.find_element_by_name("selected[]").is_selected():
-            wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[5]").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("23r23rf")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").send_keys("\\82")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("23f23r23r")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("23r23r23r")
+        wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
