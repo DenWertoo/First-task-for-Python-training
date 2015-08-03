50c199a60e6ec087d210afea41138d461c890909
DenWertoo <aldoraygreat@yandex.ru>
2015-08-03 15:25:04 +0300

    первый записанный сценарий

---
+++ first_autotest.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+success = True
+wd = WebDriver()
+wd.implicitly_wait(60)
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+try:
+    wd.get("http://localhost/addressbook/group.php")
+    wd.find_element_by_name("user").click()
+    wd.find_element_by_name("user").clear()
+    wd.find_element_by_name("user").send_keys("admin")
+    wd.find_element_by_name("pass").click()
+    wd.find_element_by_name("pass").clear()
+    wd.find_element_by_name("pass").send_keys("secret")
+    wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+    wd.find_element_by_name("new").click()
+    wd.find_element_by_name("group_header").click()
+    wd.find_element_by_name("group_name").click()
+    wd.find_element_by_name("group_name").clear()
+    wd.find_element_by_name("group_name").send_keys("EVEV")
+    wd.find_element_by_name("group_header").click()
+    wd.find_element_by_name("group_header").clear()
+    wd.find_element_by_name("group_header").send_keys("EVVE")
+    wd.find_element_by_name("group_footer").click()
+    wd.find_element_by_name("group_footer").clear()
+    wd.find_element_by_name("group_footer").send_keys("EVEVWV")
+    wd.find_element_by_name("submit").click()
+    wd.find_element_by_link_text("group page").click()
+    if not wd.find_element_by_xpath("//div[@id='content']/form/span[5]/input").is_selected():
+        wd.find_element_by_xpath("//div[@id='content']/form/span[5]/input").click()
+    if not wd.find_element_by_xpath("//div[@id='content']/form/span[4]/input").is_selected():
+        wd.find_element_by_xpath("//div[@id='content']/form/span[4]/input").click()
+    if not wd.find_element_by_xpath("//div[@id='content']/form/span[3]/input").is_selected():
+        wd.find_element_by_xpath("//div[@id='content']/form/span[3]/input").click()
+    if not wd.find_element_by_xpath("//div[@id='content']/form/span[2]/input").is_selected():
+        wd.find_element_by_xpath("//div[@id='content']/form/span[2]/input").click()
+    if not wd.find_element_by_name("selected[]").is_selected():
+        wd.find_element_by_name("selected[]").click()
+    wd.find_element_by_xpath("//div[@id='content']/form/input[5]").click()
+    wd.find_element_by_link_text("group page").click()
+    wd.find_element_by_link_text("Logout").click()
+finally:
+    wd.quit()
+    if not success:
+        raise Exception("Test failed.")
