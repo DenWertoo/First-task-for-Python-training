4bbeecf2034c9052d005094a8be5971eef06f5da
DenWertoo <aldoraygreat@yandex.ru>
2015-08-14 18:24:35 +0300

    создал тест Удаление контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -55,6 +55,17 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
+    def delete_contact(self):
+        wd = self.app.wd
+        self.open_home_page()
+        #select contact
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+        #wd.find_element_by_link_text("home").click()
+        #self.return_to_home_page()
+
     def open_home_page(self):
         wd = self.app.wd
         wd.get("http://localhost/addressbook/")
