a086508aa736afd36ef202d9483c03f28681b69c
DenWertoo <aldoraygreat@yandex.ru>
2015-08-21 15:55:55 +0300

    Валидация фикстуры перед каждым тестом

--- test/test_group_creation.py
+++ test/test_group_creation.py
@@ -3,11 +3,7 @@ from model.group import Group
 
 
 def test_group_creation(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="F1", header="F2", footer="F3"))
-    app.session.logout()
 
 def test_empty_group_creation(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
