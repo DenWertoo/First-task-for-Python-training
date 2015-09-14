f3f39e2b434a58511a753c229fe09603c0a7f2bc
DenWertoo <aldoraygreat@yandex.ru>
2015-09-14 17:46:29 +0300

    сортировка списков перед сравнением

--- test/test_group_creation.py
+++ test/test_group_creation.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_group_creation(app):
-    app.group.create(Group(name="F1", header="F2", footer="F3"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="F1", header="F2", footer="F3")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_empty_group_creation(app):
-    app.group.create(Group(name="", header="", footer=""))
+    old_groups = app.group.get_group_list()
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
